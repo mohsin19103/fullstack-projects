c22d8e89d84d71ee5602baa7cffa829daea7c042
mohsin19103 <mohsin19103@gmail.com>
2024-09-03 13:33:10 -0700

    add register controller and bug fix

--- src/index.js
+++ src/index.js
@@ -26,21 +26,3 @@ startServer();  // Start the server
 
 
 
-
-/*
-(async ()=>{
-try {
- await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
- app.on("error",(error)=>{
-  console.log("ERROR:", error)
-  throw error
- })
- app.listen(process.env.PORT,()=>{
-  console.log(`app is listeng on ${process.env.PORT}`)
- })
-}
-catch(error){
-  console.error("ERROR:",error)
-  throw err
-}
-})() */
